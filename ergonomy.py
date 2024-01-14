f8ce8ecdd6a5e0671756e84edcfbf8e7a602bbf9
gabiruca <gcsosa@espol.edu.ec>
2024-01-14 15:26:11 -0500

    Changes

--- ergonomy.py
+++ ergonomy.py
@@ -1,13 +1,25 @@
 import sys
-from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic, QtGui
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 
 class ergonomy_gui(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("ergonomy.ui",self)
         self.lineEdit.setEnabled(False)
+        self.pushButton_2.clicked.connect(self.seleccion_handler)
         #self.inadecuada()
+
+    def seleccion_handler(self):
+        self.d_box()
+
+    def d_box(self):
+        filename=QFileDialog.getOpenFileName()
+        path=filename[0]
+        print(path)
+        self.photo.setPixmap(QtGui.QPixmap(path))
+        self.photo.setScaledContents(True)
+
 
     def adecuada(self):
         self.lineEdit.setText("Postura adecuada")
